2bf23f39d7c641ea6ff17893675c7e59359f51aa
ivyxjc <ivyxjc1994@hotmail.com>
2022-07-02 16:14:29 +0800

    feat(core): load credentials key from file

--- readme.md
+++ readme.md
@@ -3,3 +3,7 @@ sam build -t template-dynamodb.yaml --config-file samconfig-dynamodb.toml
 
 sam deploy -t template-dynamodb.yaml --config-file samconfig-dynamodb.toml
 ```
+
+# Credentials
+
+**Warning: Do not use private key in `resources/credentials` in production**
--- src/main/kotlin/com/ivyxjc/kotwarden/config.kt
+++ src/main/kotlin/com/ivyxjc/kotwarden/config.kt
@@ -2,9 +2,15 @@ package com.ivyxjc.kotwarden
 
 
 import io.github.cdimascio.dotenv.dotenv
-import java.security.KeyPairGenerator
+import io.ktor.server.application.*
+import java.security.KeyFactory
+import java.security.PrivateKey
+import java.security.PublicKey
 import java.security.interfaces.RSAPrivateKey
 import java.security.interfaces.RSAPublicKey
+import java.security.spec.PKCS8EncodedKeySpec
+import java.security.spec.X509EncodedKeySpec
+import java.util.*
 
 val dotenv = dotenv {
     directory = ".env"
@@ -18,12 +24,9 @@ class Config {
         const val kdf = 0
         const val defaultValidityHours = 2L
         const val issuer = "issuer"
-        private val keyPairGen = KeyPairGenerator.getInstance("RSA")
-        private val keyPair = keyPairGen.genKeyPair()
-
         const val audience = "bitwarden"
-        val privateRsaKey: RSAPrivateKey = keyPair.private as RSAPrivateKey
-        val publicRsaKey: RSAPublicKey = keyPair.public as RSAPublicKey
+        val privateRsaKey: RSAPrivateKey = getPrivateKey() as RSAPrivateKey
+        val publicRsaKey: RSAPublicKey = getPublicKey() as RSAPublicKey
         fun isSignupAllowed(email: String): Boolean {
             // todo check whether sign up is allowed or not
             return true
@@ -31,3 +34,28 @@ class Config {
     }
 }
 
+fun getPrivateKey(): PrivateKey {
+    val key: String = Application::class.java.getResource("/credentials/private_key_pkcs8.pem")?.readText()
+        ?: error("private key not found")
+    val privateKeyPEM = key
+        .replace("-----BEGIN PRIVATE KEY-----", "")
+        .replace(System.lineSeparator().toRegex(), "")
+        .replace("-----END PRIVATE KEY-----", "")
+    val encoded = Base64.getDecoder().decode(privateKeyPEM)
+    val spec = PKCS8EncodedKeySpec(encoded)
+    val kf: KeyFactory = KeyFactory.getInstance("RSA")
+    return kf.generatePrivate(spec)
+}
+
+fun getPublicKey(): PublicKey {
+    val key: String =
+        Application::class.java.getResource("/credentials/public_key.pem")?.readText() ?: error("public key not found")
+    val privateKeyPEM = key
+        .replace("-----BEGIN PUBLIC KEY-----", "")
+        .replace(System.lineSeparator().toRegex(), "")
+        .replace("-----END PUBLIC KEY-----", "")
+    val encoded = Base64.getDecoder().decode(privateKeyPEM)
+    val spec = X509EncodedKeySpec(encoded)
+    val kf: KeyFactory = KeyFactory.getInstance("RSA")
+    return kf.generatePublic(spec)
+}
